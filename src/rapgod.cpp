693b1c74150afd90eb6a15897797a04ee14936a8
nihalmehaboob <115793246+nihalmehaboob@users.noreply.github.com>
2022-10-14 17:38:48 +0530

    b=0

--- src/rapgod.cpp
+++ src/rapgod.cpp
@@ -1,13 +1,18 @@
-#include <iostream.h>
+#include <iostream>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-  int b = 10;
+  int b = 0;
   int a = 20;
-
-  int c = a / b;
-
+if(b==0)
+{
+  cout<<"not possible";
+}
+ else
+ {
+   int c = a / b;
+ }
   cout << c;
 
   return 0;
